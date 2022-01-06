92b78fd7e7272a163b48bd0a6435348286ad6b8a
Tofik Mohammed <tofikahm@yahoo.com>
2022-01-06 05:03:48 +0000

    Updated students.js and teachers.js to use parameterized queries.

--- students.js
+++ students.js
@@ -10,10 +10,10 @@ const pool = new Pool({
 
 // Querying The Database
 pool.query(`
-SELECT id, name, cohort_id
+SELECT name, email
 FROM students
-LIMIT 5;
-`)
+WHERE id = 1;
+ `)
 .then(res => {
   console.table(res.rows);
 })
@@ -43,15 +43,22 @@ LIMIT 5;
 });
 
 // Query Parameters
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
+
